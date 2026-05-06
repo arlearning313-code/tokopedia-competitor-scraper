d06981f02f2e226634a4faef1e5bba496c75943a
arlearning313-code <arlearning313@gmail.com>
2026-05-06 12:47:53 +0700

    .

--- scraper.py
+++ scraper.py
@@ -33,7 +33,7 @@ class Product:
 
 def _init_driver() -> webdriver.Chrome:
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
